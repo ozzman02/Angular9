77693abb5700925bed37ec842d2f5a729966395c
OscarS <oscars@satellite-s55-b.local.tld>
2020-04-09 18:29:26 -0600

    crear component

---
+++ curso-angular4/src/app/empleado/empleado.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'empleado',
+    templateUrl: './empleado.component.html'
+})
+export class EmpleadoComponent{
+    public titulo = 'Componente empleado';
+}
